170c273d5ffb4a362849a9d2917baced8828c9f5
Prasanth-R-01 <prasanthramaswamy01@gmail.com>
2021-03-30 11:50:53 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <style media="screen">
+
+      .rolling-text{
+        text-align: middle;
+        padding-top: 6px;
+      }
+
+      nav ul li:hover a{
+        color: #f58634!important;
+        border-bottom-style: solid;
+        border-bottom-width: 3px;
+      }
+
+    </style>
+
+  </head>
+  <body>
+
+
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark shift">
+    <a class="navbar-brand px-3" href="#">
+      <img src="images/logo.png" alt="" width="35" height="35" class="d-inline-block align-top">
+       &ensp;  SIMMI FOUNDATION
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse text-center " id="navbarNav">
+      <ul class="navbar-nav ms-auto ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Donate</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Volunteer</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Events</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+</nav>
+
+<marquee width="100%" direction="left" height="30px" scrollamount="12" class='rolling-text'>
+  Smart India Multi Management Institute! &ensp;  स्मार्ट इंडिया बहुद्देश्यीय प्रबन्धन संस्थान! &ensp; Registration number : 085953/2020  &ensp; Niti Aayog (Govt. Of India ) Unique Id : HR/2020/0258148
+</marquee>
+
+
+  </body>
+</html>
