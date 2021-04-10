c8bf8fb75d468eb398aa1da0a3d74c0696cf1456
mohan <mohansahualbert@gmail.com>
2021-04-11 02:27:36 +0530

    created single and back to back file upload

---
+++ api_file/upload_file.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// echo" start "; 
+// http_response_code(500);
+// $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+// print_r( $_FILES); 
+// print_r( $_GET); 
+// print_r( $_REQUEST); 
+if(isset($_FILES["upload_file"])   && $_FILES["upload_file"]['size'] > 0    ){
+   sleep(2); 
+    echo json_encode(   array("status"=>"ok", "message"=>"file received" , "file name " => $_FILES["upload_file"]['name'] )); 
+    //   echo json_encode(array("status"=>"ok", "message"=>"file received" )) ; 
+}
+else{
+    sleep(2); 
+    http_response_code(500);
+    echo json_encode(array("status"=>"error", "message"=>"file not found")) ; 
+}
+// echo json_encode($age);
+// echo"end " ; 
+
+?>
--- public/js/admin_gallery_second.js
+++ public/js/admin_gallery_second.js
@@ -5,18 +5,26 @@ var main_box = document.getElementById("main_box");
 var drop_zone = document.getElementById("drop_zone");
 var browser_file_but = document.getElementById("browser_file_but"); 
 var upload_file_input = document.getElementById("upload_file_input"); 
-var upload_file_data = document.getElementById("upload_file_data"); 
+
 var disp_uploadable_file_box = document.getElementById("disp_uploadable_file_box"); 
 var disp_uploadable_file = document.getElementById("disp_uploadable_file"); 
 var upload_img_but = document.getElementById("upload_img_but"); 
 var cancel_upload_but = document.getElementById("cancel_upload_but"); 
 var cancel_upload_file_but = document.getElementById("cancel_upload_file_but"); 
+var upload_file_but = document.getElementById("upload_file_but"); 
+// var upload_img_but = document.getElementById("upload_img_but"); 
+// var upload_img_but = document.getElementById("upload_img_but"); 
+// var upload_img_but = document.getElementById("upload_img_but"); 
+
+// var upload_img_but = document.getElementById("upload_img_but"); 
+
 
 
 
 var not_upload_file_table = {}; 
 var not_upload_file_count = 0; 
-
+var upload_file_data ; 
+// var upload_img_but = document.getElementById("upload_img_but"); 
 // var upload_img_but = document.getElementById("upload_img_but"); 
 // var upload_img_but = document.getElementById("upload_img_but"); 
 // var upload_img_but = document.getElementById("upload_img_but"); 
@@ -45,7 +53,7 @@ function send_ajax(param, url, method = "post", set_header = true) {
                     resolve(xhttp.response);
                 }
                 else {
-                    reject("not able to fetch");
+                    reject(xhttp.response);
                 }
             }
         }
@@ -55,6 +63,120 @@ function send_ajax(param, url, method = "post", set_header = true) {
 
 }
 
+// send_ajax("","./api_file/upload_file.php" ).then(data=>{
+//     console.log(data); 
+// }).catch(error=>{
+//     console.log(error); 
+// })
+
+function byte_to_unit(size) {
+    if (size <= 1000) return { divi: 1, unit: "B" };
+    if (size <= 100000) return { divi: 1000, unit: "KB" };
+    if (size <= 100000000) return { divi: 1000000, unit: "MB" };
+    if (size <= 100000000000) return { divi: 1000000000, unit: "GB" };
+    if (size <= 100000000000000) return { divi: 1000000000000, unit: "TB" };
+
+
+
+}
+
+
+function upload_file_to_server(i, total_file_count,curr_file_no ) {
+
+
+   // i => i'th no of file to be uploaded 
+
+    // console.log("files aare ", upload_file_data);
+    console.log( "------------------------------------"); 
+    console.log( " i = "+ i +  " total file count   " + total_file_count + " curr file no " + curr_file_no); 
+   
+
+    // for (let i = 0; i < upload_file_data.length; i++) {
+        //generate upload id 
+
+     
+        let xhttp = new XMLHttpRequest();
+        let url = `./api_file/upload_file.php`;
+
+        // console.log("url = ", url);
+        xhttp.open("POST", url, true);
+       
+
+
+        let form_data = new FormData();
+        form_data.append("upload_file", upload_file_data[i]);
+
+
+        // xhttp.setRequestHeader("Content-type", "ap");\
+        xhttp.upload.onprogress = function (e) {
+            
+            
+                let frac = e.loaded / e.total;
+                console.log(e.loaded);
+                // element.children[0].innerText = Math.round(frac * 100) + "%";
+                // element.children[1].innerText = Math.round(e.loaded * 100 / size_detail.divi) / 100 + "/" + Math.round(e.total * 100 / size_detail.divi) / 100 + size_detail.unit;
+                // element.children[2].style.width = Math.round(frac * 100) + "%";
+                // element.children[3].style.width = Math.round(frac * 95) + "%";;
+       
+
+        }
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status >= 200 && this.status < 300) {
+                // console.log("resrpn->", this.response);
+                let res_data = JSON.parse(this.response);
+                console.log("resrpn->",res_data );
+                
+                if (res_data.status == "ok") {
+                
+                }
+                else {
+                      console.log(res_data.error);
+                }
+
+            // upload next valid file 
+            i++
+            while ( i<total_file_count ){
+                if( not_upload_file_table[i]==true ||  upload_file_data[i].size < 1   ){
+                    console.log( "---skippiing file no " + i  + "--"); 
+                    i++; 
+                }else{
+                    console.log("uploading file no " + (curr_file_no+ 1 )); 
+                    upload_file_to_server( i, total_file_count,curr_file_no+1); 
+                    break; 
+                }
+            }
+
+            }
+        }
+
+        xhttp.send(form_data);
+    }
+
+    
+
+   
+//upload button function 
+upload_file_but.addEventListener("click",    ()=>{
+
+     if (upload_file_data && upload_file_data.length > 0) {
+        // total_file_count = upload_file_data.length  - Object.keys(not_upload_file_table).length;
+    }else {
+        console.log("no filees found upload function "); 
+        return  ; 
+    }
+    let i =0; 
+    while ( i<upload_file_data.length ){
+        if( not_upload_file_table[i]==true  ||  upload_file_data[i].size < 1){
+            console.log( "---skippiing file no " + i  + "--"); 
+            i++; 
+        }else{
+            upload_file_to_server( i,upload_file_data.length,1); 
+            break; 
+        }
+    }
+
+}); 
 
 
 
@@ -71,6 +193,8 @@ cancel_upload_file_but.addEventListener("click", () => {
     disp_uploadable_file.innerHTML=""; 
     disp_uploadable_file_box.style.display="none"; 
 }); 
+
+
 
 
 browser_file_but.addEventListener("click", () => {
